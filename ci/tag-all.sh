2fe9151b2d2a5b33bf60de68d1b81ac8c5d75da1
Knut Ahlers <knut@ahlers.me>
2026-02-16 18:39:49 +0100

    chore: add tagging helper
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

---
+++ ci/tag-all.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+function log() {
+  echo "[$(date +%H:%M:%S)] $@" >&2
+}
+
+for module in $(find . -name 'go.mod' | cut -d / -f 2); do
+  log "Working in ${module}..."
+
+  tag="${module}/v$(semver-from-commits -f "${module}")"
+  log "  => Calculated tag as ${tag}"
+
+  if git tag | grep -q "${tag}"; then
+    log "  => Tag already exists, no need to tag"
+    continue
+  fi
+
+  log "  => Issuing tag..."
+  git tag "${tag}" HEAD
+done
